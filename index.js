072d42c9242637327016a550a93776220fc184f1
Adhiambo_C <cinadhiambo22@gmail.com>
2020-05-07 21:48:58 +0300

    selected movies

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const tab=document.querySelectorAll('figure')
+const tabs=document.querySelectorAll('figure')
 const tabContent= document.querySelectorAll('.movie')
+const cinema= document.querySelector('.cinema')
+const seats=document.querySelectorAll('.row .seat:not(occcupied)')
+const total =document.getElementById('total')
+const count=document.getElementById('count')
+const tickets=document.querySelectorAll('button')
+
+ 
+
 
 // show movie details
 function showTab(tabIndex){
-    tab.forEach(function(tab,i){
+    tabs.forEach(function(tab,i){
         if(tabIndex===i){
             tab.classList.add('active')
         }
@@ -13,7 +21,35 @@ function showTab(tabIndex){
     })
     tabContent.forEach(function(movie){
         movie.style.display ="none"
+        
     })
     tabContent[tabIndex].style.display="block"
 }
 showTab(2)
+
+
+//update selected seats
+function updateSelectedSeats(){
+    const selectedSeats= document.querySelectorAll('.row .seat.selected')
+    const selectedCount=selectedSeats.length
+    count.innerText= selectedCount
+    return selectedCount
+     
+}
+//button values
+function ticketPrice(index){
+    tickets.forEach(function(tick){
+        total.innerText= updateSelectedSeats()* tick.value
+    })
+    
+}
+
+// the cinema
+cinema.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+        e.target.classList.toggle('selected')
+        updateSelectedSeats()
+        ticketPrice()
+        
+    }
+})
